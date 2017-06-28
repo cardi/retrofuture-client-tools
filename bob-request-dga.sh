c1acf651322d496c8e87e03062b51c8c570de29b
Calvin Ardi <calvin@isi.edu>
2017-06-27 18:21:47 -0700

    timestamps are painful

--- bob-request-dga.sh
+++ bob-request-dga.sh
@@ -5,7 +5,7 @@ SERVER=127.0.0.1
 PORT=8000
 
 # for dga, a timestamp (that's currently ignored)
-QUERY_PAYLOAD="`date --rfc-3339=seconds`"
+QUERY_PAYLOAD="`date --rfc-3339=seconds | sed -e 's/ /T/'`"
 
 QUERY='{
   "id"       : 1,
